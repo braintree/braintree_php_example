03e452e1b8bb5396bdbbd3e650874b51e1dd813b
Braintree <code@getbraintree.com>
2021-08-11 20:00:42 +0000

    Use braintree-web-drop-in 1.31.2

--- public_html/index.php
+++ public_html/index.php
@@ -36,7 +36,7 @@
         </div>
     </div>
 
-    <script src="https://js.braintreegateway.com/web/dropin/1.31.1/js/dropin.min.js"></script>
+    <script src="https://js.braintreegateway.com/web/dropin/1.31.2/js/dropin.min.js"></script>
     <script>
         var form = document.querySelector('#payment-form');
         var client_token = "<?php echo($gateway->ClientToken()->generate()); ?>";
